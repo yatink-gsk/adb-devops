374f143b0850263cbf1d3f8bd4c540303c5edcac
Yatin Kulkarni <yatin.x.kulkarni@gsk.com>
2019-02-12 00:13:18 +0000

    Read in the Readme.md text file

--- notebooks/Users/yatin.kulkarni@gnosko.in/DemoNotebook.scala
+++ notebooks/Users/yatin.kulkarni@gnosko.in/DemoNotebook.scala
@@ -2,3 +2,8 @@
 // This is a comment
 // Take a look at the file system
 display(dbutils.fs.ls("/databricks-datasets/samples/docs/"))
+
+// Setup the textFile RDD to read the README.md file
+//   Note this is lazy 
+val textFile = sc.textFile("/databricks-datasets/samples/docs/README.md")
+
