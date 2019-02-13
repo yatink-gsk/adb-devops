735fe1e4783b7fdb3edc03a356e660c0985f8a26
Yatin Kulkarni <yatin.x.kulkarni@gsk.com>
2019-02-13 15:34:55 +0000

    Initial commit.

---
+++ notebooks/Demo3.scala
@@ -0,0 +1,12 @@
+// Databricks notebook source
+// Take a look at the file system
+display(dbutils.fs.ls("/databricks-datasets/samples/docs/"))
+
+// Setup the textFile RDD to read the README.md file
+//   Note this is lazy 
+val textFile = sc.textFile("/databricks-datasets/samples/docs/README.md")
+
+// When performing an action (like a count) this is when the textFile is read and aggregate calculated
+//    Click on [View] to see the stages and executors
+textFile.count()
+
